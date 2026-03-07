b3a5b9dd6840e0c2464ee029f3797ae34da0b5d3
mrmthornton <mrmthornton@gmail.com>
2026-03-07 17:13:26 -0500

    try debugging

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 var msg = 'Hello World';
 
 console.log(msg);
+
+function greetEveryone() {
+    console.log(greet());
+}
+
+function greet() {
+    return 'Rise and Shine, Contractors!';
+}
+
+greetEveryone();
